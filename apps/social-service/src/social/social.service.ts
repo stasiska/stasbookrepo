2a1d6b538af380acfe4920825c183b71b7fab37f
Stanislaw Kos <stas21kom12@gmail.com>
2025-04-18 13:15:11 +0300

    ADD: NEW CRUD FOR SOCIAL

--- apps/social-service/src/social/social.service.ts
+++ apps/social-service/src/social/social.service.ts
@@ -1,4 +1,4 @@
-import { AuthorIdDto, CreateGroupDto, UserIdReq,  } from '@lib/grpc/dist/typings/social_service';
+import { AuthorIdDto, CreateGroupDto, FriendDto, UserIdReq,  } from '@lib/grpc/dist/typings/social_service';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RabbitMQService } from '@lib/queue/dist/rabbitmq.service'
@@ -16,15 +16,21 @@ export class SocialService {
     async ExistFriendOrFollow(request: UserIdReq) {
         const friends = await this.prismaService.friend.findMany({
             where: {
-                requesterId: request.userId
+                accepted: true,
+                OR: [
+                    { requesterId: request.userId },
+                    { targetId: request.userId }
+                ]
             },
             select: {
+                requesterId: true,
                 targetId: true
             }
         })
         const listFriend = []
-        friends.map((friend) => listFriend.push(friend.targetId))
-
+        friends.map((friend) => friend.requesterId === request.userId ?
+            listFriend.push(friend.targetId) : listFriend.push(friend.requesterId)
+        )
         await this.queueService.emit('social.post.created', {
             authorId: request.userId,
             userIds: listFriend
@@ -36,4 +42,25 @@ export class SocialService {
         }
     }
 
+
+    async addFriend(request: FriendDto) {
+        await this.prismaService.friend.create({
+            data: {
+                requesterId: request.requesterId,
+                targetId: request.targetId
+            }
+        })
+
+        return {status: '200'}
+    }
+
+    async removeFriend(request: FriendDto) {
+        await this.prismaService.friend.deleteMany({
+            where: {
+              requesterId: request.requesterId,
+              targetId: request.targetId,
+            },
+          });
+        return {status: '200'}
+    }
 }
